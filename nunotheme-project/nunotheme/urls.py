05cf6c19282542f60190497227ba6ba62f55c7b2
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-05-07 16:11:24 -0500

    Attempt to add static files

--- nunotheme-project/nunotheme/urls.py
+++ nunotheme-project/nunotheme/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePage.as_view(), name='home'),
 ]
+urlpatterns += staticfiles_urlpatterns()
